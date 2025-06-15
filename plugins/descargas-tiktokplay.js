6524170df5713387bee3ab389401f5079f2105b3
Ado <adonaylester85@gmail.com>
2025-06-14 21:53:46 -0600

    Actualizar descargas-tiktokplay.js

--- plugins/descargas-tiktokplay.js
+++ plugins/descargas-tiktokplay.js
@@ -17,7 +17,7 @@ const handler = async (m, {
 
   let vid = json.data[0];
 
-  let caption = `*「💜」 ${vid.title}*
+  let caption = `「💜」*${vid.title}*
 
 > ✦ *Autor:* » ${vid.author}
 > ✰ *Vistas:* » ${vid.views.toLocaleString()}
